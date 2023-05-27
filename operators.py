16d9db3b0e933d42ae1f37328d804828236de451
Animesh0304 <134763456+Animesh0304@users.noreply.github.com>
2023-05-27 12:26:05 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,159 @@
+# arithmrtic operators(+)
+a=10
+b=20
+c=a+b
+print(c)
+
+# arithmrtic operators(-)
+a=10
+b=20
+c=a-b
+print(c)
+
+# arithmrtic operators(*)
+a=10
+b=20
+c=a*b
+print(c)
+
+# arithmrtic operators(/)
+a=10
+b=20
+c=a/b
+print(c)
+
+# arithmrtic operators(//)
+a=10
+b=20
+c=a//b
+print(c)
+
+# arithmrtic operators(**)
+a=10
+b=20
+c=a**b
+print(c)
+
+# arithmrtic operators(percentage)
+a=10
+b=20
+c=a/b*100
+print(c)
+
+
+# comparision operators(==)
+a=10
+b=20
+c=a==b
+print(c)
+
+# comparision operators(!=)
+a=10
+b=20
+c=a!=b
+print(c)
+
+# comparision operators(>)
+a=10
+b=20
+c=a>b
+print(c)
+
+# comparision operators(<)
+a=10
+b=20
+c=a<b
+print(c)
+
+# comparision operators(>=)
+a=10
+b=20
+c=a>=b
+print(c)
+
+# comparision operators(<=)
+a=10
+b=20
+c=a==b
+print(c)
+
+# comparision operators(==)
+a=10
+b=20
+c=a==b
+print(c)
+
+
+# assignment operators(=)
+a=10
+a=2
+print(a)
+
+# assignment operators(+=)
+a=10
+a+=2
+print(a)
+
+
+# assignment operators(-=)
+a=10
+a-=2
+print(a)
+
+
+# assignment operators(*=)
+a=10
+a*=2
+print(a)
+
+
+# assignment operators(/=)
+a=10
+a/=2
+print(a)
+
+
+# assignment operators(//=)
+a=10
+a//=2
+print(a)
+
+
+# assignment operators(**=)
+a=10
+a**=2
+print(a)
+
+# membership operators(in)
+a=[1,2,3,4,5,6,7]
+b=5
+if b in a:
+    print("exist")
+else:
+    print("sorry")
+
+# membership operators(not in)
+a=[1,2,3,4,5,6,7]
+b=50
+if b in a:
+    print("exist")
+else:
+    print("sorry")
+
+# identify operators(is)
+a=10
+b=10
+c= a is b
+print("c")
+
+# identify operators(is not)
+a=10
+b=10
+c= a is not b
+print("c")
+
+
+
+
+
+
